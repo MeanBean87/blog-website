89db734401827c0b1c7683715b5f75e470d8bf6c
Michael Mattingly <mikeamatt1@hotmail.com>
2023-08-09 12:02:36 -0400

    Created comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const commentHandler = async (event) => {
+    event.preventDefault();
+
+    const comment = document.querySelector('#comment_content').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment) {
+        const response = await fetch(`/api/comments`, {
+            method: 'POST',
+            body: JSON.stringify({ comment, post_id }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+ 
+document.querySelector('#comment').addEventListener('submit', commentHandler);
